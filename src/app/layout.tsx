d6dea1d5c446fa634eaeefe0d644e9a727e9b99d
jintao <hellojintao@protonmail.com>
2024-02-16 00:57:31 -0600

    chore: update test repo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,39 +2,6 @@
 import '@/styles/globals.css'
 import '@rainbow-me/rainbowkit/styles.css'
 import React from 'react'
-import { Inter } from 'next/font/google'
-import { configureChains, createConfig, WagmiConfig } from 'wagmi'
-import { createPublicClient, http } from 'viem'
-import { arbitrum, mainnet } from 'wagmi/chains'
-import { alchemyProvider } from 'wagmi/providers/alchemy'
-import { publicProvider } from 'wagmi/providers/public'
-import { getDefaultWallets, RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit'
-import Navbar from '@/components/Navbar'
-
-const inter = Inter({ subsets: ['latin'] })
-
-// // WAGMI Chains
-const { chains } = configureChains(
-  [mainnet, arbitrum],
-  [alchemyProvider({ apiKey: process.env.ALCHEMY_API_KEY }), publicProvider()],
-)
-
-const projectId = 'WalletConnectID(NeedsReplacing)'
-
-const { connectors } = getDefaultWallets({
-  appName: 'EVM-FE-Boilerplate',
-  projectId,
-  chains,
-})
-
-const config = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient: createPublicClient({
-    chain: mainnet,
-    transport: http(),
-  }),
-})
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -46,14 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel='icon' type='image/png' sizes='64x64' href='/favicon.png' />
       </head>
       <html lang='en'>
-        <body className={`${inter.className}`}>
-          <WagmiConfig config={config}>
-            <RainbowKitProvider chains={chains} modalSize='compact' theme={darkTheme()}>
-              <Navbar />
-              {children}
-            </RainbowKitProvider>
-          </WagmiConfig>
-        </body>
+        <body>{children}</body>
       </html>
     </>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,13 @@
 import Link from 'next/link'
 
-// Landing page localhost:3000/
+// Onboard
+// https://onboard.blocknative.com/#get-started
 
 export default function LandingPage() {
   return (
     <>
       <main className='flex min-h-screen flex-col items-center justify-center'>
-        <p className='rounded-xl border border-slate-500 bg-gradient-to-b from-zinc-800/30 to-zinc-500/40 p-4'>
-          EVM-FE-Bootstrap Repository
-        </p>
-        <Link href='/examplePage'>
-          <div className='mt-3 rounded-xl border border-slate-500 bg-gradient-to-b p-3 hover:from-zinc-800/40 hover:to-zinc-500/50'>
-            Example Page
-          </div>
-        </Link>
+        <span className='text-3xl'>Jintao Test!</span>
       </main>
     </>
   )
